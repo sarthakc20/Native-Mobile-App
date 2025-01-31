2a6f8eae818431b175f32e8c6169bbfae9d85158
Sarthak Chatterjee <sarthatc@gmail.com>
2025-01-31 15:54:27 +0530

    Post screen created and added post functionality

--- client/Context/authContext.js
+++ client/Context/authContext.js
@@ -25,7 +25,7 @@ const AuthProvider = ({ children }) => {
 
   //default axios setting
   axios.defaults.headers.common['Authorization'] = `Bearer ${state?.token}`;
-  axios.defaults.baseURL = "http://192.168.1.6:8080/api/v1";
+  axios.defaults.baseURL = "http://192.168.1.14:8080/api/v1";
 
   return (
     <AuthContext.Provider value={[state, setState]}>
